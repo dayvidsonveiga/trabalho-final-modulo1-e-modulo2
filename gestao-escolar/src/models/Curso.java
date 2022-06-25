feebbcd2bc439c6fda85f6e92584c11dcf041232
cassiafg <cassiafguimaraes@gmail.com>
2022-06-24 23:07:41 -0300

    classes Estudante e Curso - atributos

---
+++ gestao-escolar/src/models/Curso.java
@@ -0,0 +1,6 @@
+package models;
+
+public class Curso {
+    private String curso;
+    Double nota1, nota2, media;
+}
---
+++ gestao-escolar/src/models/Estudante.java
@@ -0,0 +1,6 @@
+package models;
+
+public class Estudante {
+    private Integer matricula, tipo;
+    private Curso curso;
+}
