c830872d5326cd93f28dd65f0364f4c5de73d1b7
Jean <jean.silva@dbccompany.com.br>
2022-06-25 02:00:04 -0300

    feat: implementando a classe Menu

---
+++ gestao-escolar/src/Main.java
@@ -0,0 +1,13 @@
+import models.Menu;
+
+import javax.swing.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Boolean controle = true;
+        Menu menu = new Menu();
+        menu.menuPrincipal();
+
+    }
+}
---
+++ gestao-escolar/src/models/Menu.java
@@ -0,0 +1,173 @@
+package models;
+
+import java.util.Scanner;
+
+public class Menu {
+    Scanner scanner = new Scanner(System.in);
+    Integer opcao;
+    Boolean controle = true;
+
+//    public void limpaTela() {
+//        Runtime.getRuntime().exec("cls");
+//        Runtime.getRuntime().exec("clear");
+//    }
+    public void menuPrincipal(){
+        while (controle) {
+            System.out.println("##SEJA BEM VINDO A ESCOLA##");
+            System.out.println("---------------------------");
+            System.out.println("Digite 1 para acessar Portal do Aluno");
+            System.out.println("Digite 2 para acessar Portal do Colaborador");
+            System.out.println("Digite 0 para sair");
+            opcao = Integer.parseInt(scanner.nextLine());
+            switch (opcao) {
+                case 0 -> {
+                    controle = false;
+                    break;
+                }
+                case 1 -> {
+                    menuPortalDoAluno();
+                }
+                case 2 -> {
+                    menuPortalDoColaborador();
+                }
+                default -> {
+                    System.out.println("Opção inválida");
+                }
+            }
+        }
+    }
+    public void menuPortalDoAluno() {
+        System.out.println("Digite 1 para conferir as notas:");
+        System.out.println("Digite 2 para voltar");
+        System.out.println("Digite 0 para sair");
+        opcao = Integer.parseInt(scanner.nextLine());
+        switch (opcao) {
+            case 0 -> {
+                controle = false;
+                break;
+            }
+            case 1 -> {}
+            case 2 -> {
+                menuPrincipal();
+
+            }
+        }
+
+    }
+
+    public void menuPortalDoColaborador() {
+//        limpaTela();
+        System.out.println("Digite 1 para acessar Área do Colaborador");
+        System.out.println("Digite 2 Para acessar SysAdmin");
+        System.out.println("Digite 3 para voltar");
+        System.out.println("Digite 0 para sair");
+        opcao = Integer.parseInt(scanner.nextLine());
+        switch (opcao) {
+            case 0 -> {
+                controle = false;
+                break;
+            }
+            case 1 -> {menuAreaDoColaborador();}
+            case 2 -> {menuSysAdmin();}
+            case 3 -> {}
+        }
+    }
+    public void menuAreaDoColaborador() {
+//        limpaTela();
+        System.out.println("Digite 1 para conferir os últimos 3 pagamentos:");
+        System.out.println("Digite 2 para voltar para o Menu Principal");
+        System.out.println("Digite 0 para sair");
+        opcao = Integer.parseInt(scanner.nextLine());
+        switch (opcao) {
+            case 0 -> {
+                controle = false;
+                break;
+            }
+            case 1 -> {}
+            case 2 -> {
+                menuPrincipal();
+
+            }
+        }
+    }
+
+    public void menuSysAdmin() {
+//        limpaTela();
+        System.out.println("Digite 1 para gerenciar alunos");
+        System.out.println("Digite 2 para gerenciar colaboradores");
+        System.out.println("Digite 3 para voltar ao Menu Principal");
+        System.out.println("Digite 0 para sair");
+        opcao = Integer.parseInt(scanner.nextLine());
+        switch (opcao) {
+            case 0 -> {
+                controle = false;
+                break;
+            }
+            case 1 -> {
+                menuGerenciarAlunos();
+            }
+            case 2 -> {
+                menuGerenciarColaboradores();
+
+            }
+            case 3 -> {
+                menuPrincipal();
+            }
+        }
+    }
+
+    public void menuGerenciarAlunos() {
+//        limpaTela();
+        System.out.println("Digite 1 para adicionar um aluno");
+        System.out.println("Digite 2 para atualizar as informações de um aluno");
+        System.out.println("Digite 3 para remover um aluno");
+        System.out.println("Digite 4 para acessar informações de um aluno");
+        System.out.println("Digite 5 para voltar ao Menu Principal");
+        System.out.println("Digite 0 para sair");
+        opcao = Integer.parseInt(scanner.nextLine());
+        switch (opcao) {
+            case 0 -> {
+                controle = false;
+                break;
+            }
+            case 1 -> {
+            }
+            case 2 -> {
+            }
+            case 3 -> {
+            }
+            case 4 -> {}
+            case 5 -> {
+                menuPrincipal();
+            }
+        }
+    }
+
+    public void menuGerenciarColaboradores() {
+//        limpaTela();
+        System.out.println("Digite 1 para adicionar um colaborador");
+        System.out.println("Digite 2 para atualizar as informações de um colaborador");
+        System.out.println("Digite 3 para remover um colaborador");
+        System.out.println("Digite 4 para acessar informações de um colaborador");
+        System.out.println("Digite 5 para voltar ao Menu Principal");
+        System.out.println("Digite 0 para sair");
+        opcao = Integer.parseInt(scanner.nextLine());
+        switch (opcao) {
+            case 0 -> {
+                controle = false;
+                break;
+            }
+            case 1 -> {
+            }
+            case 2 -> {
+            }
+            case 3 -> {
+            }
+            case 4 -> {}
+            case 5 -> {
+                menuPrincipal();
+            }
+        }
+    }
+}
+
